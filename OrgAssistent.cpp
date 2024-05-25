2794b25d0186d6cde20a2371420f488162d01a31
Aleksa <80622833+AleksaMilosevic07@users.noreply.github.com>
2024-05-25 20:29:35 +0200

    Organization Folder setup
    Setup function now works. When you select your Organization folder, the code will create multiple folders in that folder for all different types of categories of files.

--- OrgAssistent.cpp
+++ OrgAssistent.cpp
@@ -42,7 +42,20 @@ void setup()
     cout<<"\n\n\n";
     cout<<"First select the path where you want Organization Assistant to make his folders. This includes text files, document files, etc...\n";
     orgFolder();
+    fs::create_directories(orgPath + "/Texts");
+    cout<<"Folder for textual files created at "<<orgPath<<"\Texts\n";
+    fs::create_directories(orgPath + "/Photos");
+    cout<<"Folder for grahic files created at "<<orgPath<<"\Photos\n";
+    fs::create_directories(orgPath + "/Audio");
+    cout<<"Folder for audio files created at "<<orgPath<<"\Audio\n";
+    fs::create_directories(orgPath + "/Videos");
+    cout<<"Folder for video files created at "<<orgPath<<"\Videos\n";
+    fs::create_directories(orgPath + "/CodeFiles");
+    cout<<"Folder for code files created at "<<orgPath<<"\CodeFiles\n";
+    fs::create_directories(orgPath + "/Archives");
+    cout<<"Folder for archive files created at "<<orgPath<<"\Archives\n\n";
     unOrgFolder();
+    cout<<"All set! You are free to proceed!\n\n";
 }
 
 void startup()
