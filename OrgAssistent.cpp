d39680f67054bee33911111a18ceb716eca4b9ea
Aleksa <80622833+AleksaMilosevic07@users.noreply.github.com>
2024-05-31 18:57:32 +0200

    Finished product (I hope)
    Finished the main function for moving files. Also made a lot of improvements, some of using "path" instead of string to store the target addresses.
    
    I have also made a block for catching errors while moving files (if it fails)
    
    Only testing and commenting is to be done

--- OrgAssistent.cpp
+++ OrgAssistent.cpp
@@ -1,95 +1,169 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <filesystem>
-using namespace std;
+#include <string>
+
 namespace fs = std::filesystem;
-bool sigma = 0, orgPathExist = 0, unOrgPathExist = 0;
-string orgPath, unOrgPath;
+using namespace std;
+
+bool sigma = false, orgPathExist = false, unOrgPathExist = false;
+fs::path orgPath, unOrgPath;
 
 void orgFolder()
 {
-    cout<<"\n";
-    cout<<"Path to your organization folder: ";
-    cin>>orgPath;
-    if(fs::exists(orgPath)) cout<<"Found a file on specified path, proceed.\n";
+    cout << "\nPath to your organization folder: ";
+    cin >> orgPath;
+    if (fs::exists(orgPath))
+        cout << "Found a folder on specified path, proceed.\n";
     else
     {
-        cout<<"Invalid path location, please try again";
+        cout << "Invalid path location, please try again.\n";
         orgFolder();
     }
-    orgPathExist = 1;
-
+    orgPathExist = true;
 }
 
 void unOrgFolder()
 {
-    cout<<"\n";
-    cout<<"Path to your unorganized folder: ";
-    cin>>unOrgPath;
-    if(fs::exists(unOrgPath)) cout<<"Found a file on specified path, proceed.\n";
+    cout << "\nPath to your unorganized folder: ";
+    cin >> unOrgPath;
+    if (fs::exists(unOrgPath))
+        cout << "Found a folder on specified path, proceed.\n";
     else
     {
-        cout<<"Invalid path location, please try again";
-        orgFolder();
+        cout << "Invalid path location, please try again.\n";
+        unOrgFolder();
     }
-    unOrgPathExist = 1;
+    unOrgPathExist = true;
 }
 
 void start()
 {
-
-    for(const auto& entry : fs::directory_iterator(unOrgPath))
+    for (const auto& entry : fs::directory_iterator(unOrgPath))
     {
-       string ext = entry.path().extension().string();
-       if(ext == ".txt" or ext == ".docx" or ext == ".pdf" or ext == ".rtf" or ext == ".csv")
-       {
+        string ext = entry.path().extension().string();
+
+        //Text files
+        if (ext == ".txt" || ext == ".docx" || ext == ".pdf" || ext == ".rtf" || ext == ".csv")
+        {
             try
             {
-                fs::rename(entry.path(), orgPath);
+                fs::path newPath = orgPath / "Texts" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
             }
-            catch(const fs::filesystem_error& e)
+            catch (const fs::filesystem_error& e)
             {
-                cout<<"Error while moving files\n";
+                cerr << "Error while moving files: " << e.what() << endl;
             }
-       }
+        }
+        //Photos
+        else if (ext == ".jpg" || ext == ".png" || ext == ".gif" || ext == ".bmp" || ext == ".svg")
+        {
+            try
+            {
+                fs::path newPath = orgPath / "Photos" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
+            }
+            catch (const fs::filesystem_error& e)
+            {
+                cerr << "Error while moving files: " << e.what() << endl;
+            }
+        }
+        //Audio
+        else if (ext == ".mp3" || ext == ".wav" || ext == ".flac" || ext == ".aac" || ext == ".ogg")
+        {
+            try
+            {
+                fs::path newPath = orgPath / "Audio" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
+            }
+            catch (const fs::filesystem_error& e)
+            {
+                cerr << "Error while moving files: " << e.what() << endl;
+            }
+        }
+        //Videos
+        else if (ext == ".mp4" || ext == ".avi" || ext == ".mov" || ext == ".mkv" || ext == ".wmv")
+        {
+            try
+            {
+                fs::path newPath = orgPath / "Videos" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
+            }
+            catch (const fs::filesystem_error& e)
+            {
+                cerr << "Error while moving files: " << e.what() << endl;
+            }
+        }
+        //CodeFiles
+        else if (ext == ".cpp" || ext == ".c" || ext == ".java" || ext == ".py" || ext == ".html" || ext == ".css"|| ext == ".lua" || ext == ".js")
+        {
+            try
+            {
+                fs::path newPath = orgPath / "CodeFiles" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
+            }
+            catch (const fs::filesystem_error& e)
+            {
+                cerr << "Error while moving files: " << e.what() << endl;
+            }
+        }
+        //Archives
+        else if (ext == ".zip" || ext == ".rar" || ext == ".tar.gz" || ext == ".7z" || ext == ".iso")
+        {
+            try
+            {
+                fs::path newPath = orgPath / "Archives" / entry.path().filename();
+                fs::rename(entry.path(), newPath);
+                cout << "File " << entry.path() << " moved to " << newPath << endl;
+            }
+            catch (const fs::filesystem_error& e)
+            {
+                cerr << "Error while moving files: " << e.what() << endl;
+            }
+        }
 
     }
 }
 
 void setup()
 {
-    cout<<"\n\n\n";
-    cout<<"First select the path where you want Organization Assistant to make his folders. This includes text files, document files, etc...\n";
+    cout << "\n\n\n";
+    cout << "First select the path where you want Organization Assistant to make his folders. This includes text files, document files, etc...\n";
     orgFolder();
-    fs::create_directories(orgPath + "/Texts");
-    cout<<"Folder for textual files created at "<<orgPath<<"\Texts\n";
-    fs::create_directories(orgPath + "/Photos");
-    cout<<"Folder for grahic files created at "<<orgPath<<"\Photos\n";
-    fs::create_directories(orgPath + "/Audio");
-    cout<<"Folder for audio files created at "<<orgPath<<"\Audio\n";
-    fs::create_directories(orgPath + "/Videos");
-    cout<<"Folder for video files created at "<<orgPath<<"\Videos\n";
-    fs::create_directories(orgPath + "/CodeFiles");
-    cout<<"Folder for code files created at "<<orgPath<<"\CodeFiles\n";
-    fs::create_directories(orgPath + "/Archives");
-    cout<<"Folder for archive files created at "<<orgPath<<"\Archives\n\n";
+    fs::create_directories(orgPath / "Texts");
+    cout << "Folder for textual files created at " << (orgPath / "Texts").string() << "\n";
+    fs::create_directories(orgPath / "Photos");
+    cout << "Folder for graphic files created at " << (orgPath / "Photos").string() << "\n";
+    fs::create_directories(orgPath / "Audio");
+    cout << "Folder for audio files created at " << (orgPath / "Audio").string() << "\n";
+    fs::create_directories(orgPath / "Videos");
+    cout << "Folder for video files created at " << (orgPath / "Videos").string() << "\n";
+    fs::create_directories(orgPath / "CodeFiles");
+    cout << "Folder for code files created at " << (orgPath / "CodeFiles").string() << "\n";
+    fs::create_directories(orgPath / "Archives");
+    cout << "Folder for archive files created at " << (orgPath / "Archives").string() << "\n\n";
     unOrgFolder();
-    cout<<"All set! You are free to proceed!\n\n";
+    cout << "All set! You are free to proceed!\n\n";
 }
 
 void startup()
 {
     int a;
-    cout<<"\n\n\n";
-    cout<<"Welcome to Organization Assistant. This program will move all your files in the target folder to the folders you tell them to. \n";
-    cout<<"Select one of the options bellow: \n";
-    cout<<"0. Exit program \n1. Setup \n2. Organization folder path \n3. Unorganized folder path \n4. Start\n";
+    cout << "\n\n\n";
+    cout << "Welcome to Organization Assistant. This program will move all your files in the target folder to the folders you tell them to. \n";
+    cout << "Select one of the options below: \n";
+    cout << "0. Exit program \n1. Setup \n2. Organization folder path \n3. Unorganized folder path \n4. Start\n";
 
-
-    cin>>a;
-    switch(a)
+    cin >> a;
+    switch (a)
     {
         case 0:
-            sigma = 1;
+            sigma = true;
             break;
         case 1:
             setup();
@@ -101,23 +175,22 @@ void startup()
             unOrgFolder();
             break;
         case 4:
-            if(orgPathExist and unOrgPathExist)
+            if (orgPathExist && unOrgPathExist)
             {
                 start();
             }
-            else cout<<"There is no set path to either Organization folder or the Unorganized folder. Try again.\n";
+            else
+                cout << "There is no set path to either Organization folder or the Unorganized folder. Try again.\n";
             break;
         default:
-            cout<<"Invalid answer, try again.\n";
+            cout << "Invalid answer, try again.\n";
             break;
     }
-
-
 }
 
 int main()
 {
-    while(!sigma)
+    while (!sigma)
     {
         startup();
     }
